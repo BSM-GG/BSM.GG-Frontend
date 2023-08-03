18aaa4615a9ffdec5082d4bdfb3a1a8ec845f2d4
ddd2341 <rlawnsrud0509@gmail.com>
2023-08-04 06:23:26 +0900

    feat : Add header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import { styled } from "styled-components";
+import Logo from "assets/logo/BSM/GG.svg";
+
+const Header = () => {
+
+}
+
+export default Header;
+
+const container = styled.div`
+    width: 100vw;
+    height: 5rem;
+
+    display: flex;
+    justify-content: space-between;
+
+
+`;
---
+++ src/components/svg/mainLogo.tsx
@@ -0,0 +1,5 @@
+import SVGAttributeProps from "../../types/SVGAttributeProps.type";
+
+const MainLogo = ({ width = 7.438, height = 2.25 }: SVGAttributeProps) => {
+    
+};
