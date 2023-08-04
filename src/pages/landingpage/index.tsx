15c0b1e8035ef5cb6d881532df608383b31cc2c8
ddd2341 <rlawnsrud0509@gmail.com>
2023-08-04 09:04:27 +0900

    feat : Add globalstyle center align

---
+++ src/pages/landingpage/index.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components"; \
+import * as C from "../../components/index"
+
+const LandingPage = () => {
+    return (
+        <>
+            <C.Header />
+
+        </>
+    )
+}
+
+export default LandingPage
+
+const Container = styled.div`
+    
+`;
