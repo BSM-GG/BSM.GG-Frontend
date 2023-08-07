6930477d3506a4d5bd867c61b9e77593106978c1
ddd2341 <rlawnsrud0509@gmail.com>
2023-08-07 17:08:38 +0900

    feat : Change default size

--- src/components/flex/index.tsx
+++ src/components/flex/index.tsx
@@ -1,19 +1,31 @@
+import { ReactNode } from "react";
 import styled from "styled-components";
 import FlexBoxAttribute from "../../types/FlexBoxAttributeProps.type";
 
-const Flex = ({
-  direction = "row",
-  justifyContent = "flex-start",
-  alignItems = "flex-start",
-  gap = 0,
-}: FlexBoxAttribute) => {
-  return <FlexBox FlexInfo={{ direction, justifyContent, alignItems, gap }} />;
+const Flex = (
+  {
+    direction = "row",
+    justifyContent = "flex-start",
+    alignItems = "flex-start",
+    gap = 0,
+  }: FlexBoxAttribute,
+  children: ReactNode
+) => {
+  return (
+    <FlexBox FlexInfo={{ direction, justifyContent, alignItems, gap }}>
+      {children}
+    </FlexBox>
+  );
 };
 
 export default Flex;
 
 const FlexBox = styled.div<{ FlexInfo: FlexBoxAttribute }>`
+  width: max-content;
+  height: max-content;
+
   display: flex;
+
   flex-direction: ${(props) => props.FlexInfo.direction};
   justify-content: ${(props) => props.FlexInfo.justifyContent};
   align-items: ${(props) => props.FlexInfo.direction};
