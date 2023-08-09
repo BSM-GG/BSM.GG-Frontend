a67c385f261ce28aca94ace1eabd9f02bfafef1d
Ubinquitous <ubinquitous1@gmail.com>
2023-08-10 08:42:12 +0900

    feat, fix : setting router and divide to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Header } from "./components";
+import { ROUTER } from "./constants/router.constant";
+import DuoRequestPage from "./pages/duo-requestpage";
+import LandingPage from "./pages/landingpage";
 import UserPage from "./pages/userpage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path={ROUTER.HOME} element={<LandingPage />} />
+      <Route path={ROUTER.USER} element={<UserPage />} />
+      <Route path={ROUTER.DUO_REQUEST} element={<DuoRequestPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 function App() {
-  return <UserPage></UserPage>;
+  return (
+    <>
+      <Header />
+      <Router />
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default App;
